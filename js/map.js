000c918620ff09b6f06b67ae027c37b140faad0b
Ing. Johany <johany87@gmail.com>
2017-02-23 12:49:39 -0500

    add map without marker and eliminated images

---
+++ js/map.js
@@ -0,0 +1,8 @@
+function myMap() {
+    var mapDiv = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(4.6676, -74.1171),
+        zoom: 17
+    };
+    var map = new google.maps.Map(mapDiv, mapOptions);
+}
