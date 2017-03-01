eaa5f30e0ceb92ccb101ea9f1fa084395898653e
Ing. Johany <johany87@gmail.com>
2017-02-28 19:56:59 -0500

    delete css files leftover
    delete css files leftover

--- php/contacto.php
+++ php/contacto.php
@@ -1,20 +1,20 @@
 <?php
-  $nombre = $_POST['name'];
-  $email = $_POST['email'];
-  $mensaje = $_POST['mensaje'];
-  $tema = $_POST['tema'];
-  $para = 'johany87@gmail.com'
-  $titulo = 'WEB CONTACT';
-  $header = 'From: ' . $email;
-  $msjcorreo = 'Nombre: $nombre\n e-mail: $email\n Mensaje: $mensaje';
+      $nombre = $_POST["nombre"];
+      $email = $_POST["email"];
+      $mensaje = $_POST["mensaje"];
+      $tema = $_POST["tema"];
+      $para = "johany87@gmail.com"
+      $titulo = "WEB CONTACT";
+      $header = "From: " . $email;
+      $msjcorreo = "Nombre: $nombre\n e-mail: $email\n Mensaje: $mensaje";
 
-  if ($_POST['submit']) {
+      if ($_POST["submit"]) {
     if(mail($para, $titulo, $msjcorreo, $header)){
-      echo "<script lenguage='javascript'>
-        alert('Mensaje enviado correctamente, muchas gracias.');
+          echo "<script lenguage="javascript">
+            alert("Mensaje enviado correctamente, muchas gracias.");
         </script>";
     }else {
-      echo 'El mensaje no fue enviado';
+          echo "El mensaje no fue enviado";
     }
   }
  ?>
