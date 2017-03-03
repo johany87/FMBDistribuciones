53dc7c9e5628179b9d6cd2b8a88b9f46652259f3
Ing. Johany <johany87@gmail.com>
2017-03-03 18:02:59 -0500

    add contact button
    add contact button

--- php/contacto.php
+++ php/contacto.php
@@ -1,21 +1,25 @@
 <?php
       if (isset($_POST["enviar"])) {
-            $nombre = $_POST["nombre"];
-            $email = $_POST["email"];
-            $tema = $_POST["tema"];
-            $mensaje = $_POST["mensaje"];
-            $para = "johany87@gmail.com"
-            $titulo = "WEB CONTACT";
-            $headers = "From: " . $email;
-            $headers .= "X-Mailer: PHP5\n";
-            $headers .= 'MIME-Version: 1.0' . "\n";
-            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; //
-            $msjcorreo = "Nombre: $nombre\n e-mail: $email\n Mensaje: $mensaje";
-            if (mail($para, $asunto, $msjcorreo, $headers)) {
-                  echo "Mensaje enviado correctamente";
-                  echo "<a href='https://calm-lake-89825.herokuapp.com/index.html'></a>";
-            }else {
-                  echo "Mensaje no enviado, intente luego";
-            }
+       $nombre = $_POST["nombre"];
+       $email = $_POST["email"];
+       $tema = $_POST["tema"];
+       $mensaje = $_POST["mensaje"];
+       $para = "johany87@gmail.com";
+       $headers = "From: " .$email;
+       $headers .= " X-Mailer: PHP5\n";
+       $headers .= ' MIME-Version: 1.0' . "\n";
+       $headers .= ' Content-type: text/html; charset=iso-8859-1' . "\r\n";
+       $msjcorreo = "Nombre: $nombre\n e-mail: $email\n Mensaje: $mensaje";
+        if (mail($para, $tema, $msjcorreo, $headers)) {
+         echo "<script type='text/javascript'>
+         alert('Mensaje enviado correctamente');
+         history.back(1);
+         </script>";
+        }else {
+         echo "<script type='text/javascript'>
+         alert('Problemas al enviar, intentenuevamente');
+         history.back(1);
+         </script>";
+        }
       }
- ?>
+?>
